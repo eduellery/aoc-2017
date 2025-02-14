7ce02168156e8c585f21b138b17491c82f4a3c93
Eduardo Ellery <eduellery@gmail.com>
2025-02-14 13:52:47 +0100

    Remove old files

--- code/src/main/java/Day01.java
+++ code/src/main/java/Day01.java
@@ -3,7 +3,7 @@ import java.util.stream.IntStream;
 public record Day01(int part1, int part2) {
 
     public Day01(String input) {
-        this(solvePart1(input), 2);
+        this(solvePart1(input), solvePart2(input));
     }
 
     private static int solvePart1(String input) {
@@ -12,6 +12,17 @@ public record Day01(int part1, int part2) {
 
         return IntStream.range(0, length)
                 .filter(i -> digits[i] == digits[(i + 1) % length])
+                .map(i -> digits[i])
+                .sum();
+    }
+
+    private static int solvePart2(String input) {
+        int[] digits = input.chars().map(Character::getNumericValue).toArray();
+        int length = digits.length;
+        int step = length / 2;
+
+        return IntStream.range(0, length)
+                .filter(i -> digits[i] == digits[(i + step) % length])
                 .map(i -> digits[i])
                 .sum();
     }
--- code/src/test/java/Day01Test.java
+++ code/src/test/java/Day01Test.java
@@ -1,10 +1,13 @@
 import java.util.stream.Stream;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 class Day01Test {
 
     private String fileName = "day01.in";
@@ -15,6 +18,16 @@ class Day01Test {
             Arguments.of("1111", 4),
             Arguments.of("1234", 0),
             Arguments.of("91212129", 9)
+        );
+    }
+
+    private static Stream<Arguments> provideExamplesPart2() {
+        return Stream.of(
+            Arguments.of("1212", 6),
+            Arguments.of("1221", 0),
+            Arguments.of("123425", 4),
+            Arguments.of("123123", 12),
+            Arguments.of("12131415", 4)
         );
     }
 
@@ -31,4 +44,19 @@ class Day01Test {
         Day01 day = new Day01(input);
         assertEquals(1343, day.part1());
     }
+
+    @ParameterizedTest
+    @MethodSource("provideExamplesPart2")
+    void testExamplePart2(String input, int expected) {
+        Day01 day = new Day01(input);
+        assertEquals(expected, day.part2());
+    }
+
+    @Test
+    void testSolutionPart2() {
+        String input = Utils.readLine(fileName, Day01Test.class);
+        Day01 day = new Day01(input);
+        assertEquals(1274, day.part2());
+    }
+
 }
